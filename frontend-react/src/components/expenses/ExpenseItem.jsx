b7c38b6e4dd6e6c7d5d999c98b0a34af47ec73bc
Esteban Giraldo <Estebangiraldollano@gmail.com>
2026-02-16 11:59:53 -0500

    feat: add delete button & logic

--- frontend-react/src/components/expenses/ExpenseItem.jsx
+++ frontend-react/src/components/expenses/ExpenseItem.jsx
@@ -1,10 +1,14 @@
-export default function ExpenseItem({ expense }) {
+export default function ExpenseItem({ expense, onDelete }) {
     return (
         <div>
             <p>{expense.amount}</p>
             <p>{expense.category}</p>
             <p>{expense.description}</p>
             <p>{expense.date}</p>
+
+            <button onClick={() => onDelete(expense.id)}>
+                <i className="fa-solid fa-trash-can"></i>
+            </button>
         </div>
     );
 }
