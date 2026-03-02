7e72bb5dca6312be307ebe8d93f42d1594974027
Esteban Giraldo <Estebangiraldollano@gmail.com>
2026-03-02 13:38:02 -0500

    refactor: change theme switch to a component

--- frontend-react/src/components/layout/Header.jsx
+++ frontend-react/src/components/layout/Header.jsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "../ui/ThemeSwitch";
 import "./Header.css"
 
 export default function Header() {
     return (
         <header>
             <h1>Expense Tracker</h1>
+            <ThemeToggle />
         </header>
     );
 }
