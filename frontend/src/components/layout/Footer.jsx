1d97ef1e59152655e1a7cc921ad19d8d876e4314
Esteban Giraldo <Estebangiraldollano@gmail.com>
2026-03-04 11:28:16 -0500

    feat: add basic footer

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css"
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>
+                © 2026 • Egiraldol {" "}
+                <a href="https://github.com/egiraldol" target="_blank" rel="noopener noreferrer">
+                GitHub
+                </a>
+                {" | "}
+                <a href="https://www.linkedin.com/in/esteban-giraldo-llano-490565267/" target="_blank" rel="noopener noreferrer">
+                LinkedIn
+                </a>
+            </p>
+        </footer>
+    );
+}
